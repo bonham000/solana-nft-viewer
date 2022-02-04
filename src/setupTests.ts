25c42c18d08fdc3ef2c0c32644cdcd47b22c328e
Sean Smith <sean.smith.2009@gmail.com>
2022-02-04 13:11:06 -0600

    Add unit test for web3 utils

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+// Mock browser fetch API
+// https://github.com/jefflau/jest-fetch-mock
+require("jest-fetch-mock").enableMocks();
---
+++ src/test/web3.test.ts
@@ -0,0 +1,97 @@
+import fetch from "jest-fetch-mock";
+import { fetchActivityHistoryForMintAddress } from "../tools/web3";
+
+describe("web3 tests", () => {
+  /**
+   * For some reason the @metaplex/js library fails to return some of the
+   * NFT metadata when running in the NodeJS/Jest environment. Mock the
+   * metadata response here so the test can proceed.
+   */
+  beforeEach(() => {
+    const metadata = {
+      name: "MBB #2047",
+      symbol: "MBB",
+      description:
+        "Monkey Baby Business is a collection of 3,000 generative monkey baby NFTs inspired by Solana Monkey Business and the MonkeDAO. Created by a Monke, for Monkes, with 100% of all proceeds and royalties donated to the MonkeDAO.",
+      seller_fee_basis_points: 500,
+      image:
+        "https://www.arweave.net/luuNTv5eE5oN8uh-F6U5f4aXOF9WUGfyLrsyF6ivSHM?ext=png",
+      collection: { name: "MBB", family: "MBB" },
+      properties: {
+        files: [
+          {
+            uri: "https://www.arweave.net/luuNTv5eE5oN8uh-F6U5f4aXOF9WUGfyLrsyF6ivSHM?ext=png",
+            type: "image/png",
+          },
+        ],
+        category: "image",
+        creators: [
+          {
+            address: "CATPa1APxUC7L3KaWkR2Aiy5LYBThRiNQM1MFrTDC9ZC",
+            share: 100,
+          },
+        ],
+      },
+      attributes: [
+        { trait_type: "Background", value: "Pink" },
+        { trait_type: "Skin", value: "Purple" },
+        { trait_type: "Clothes", value: "Red Shirt" },
+        { trait_type: "Headwear", value: "Ninja Bandanna" },
+      ],
+    };
+
+    fetch.mockOnce(JSON.stringify(metadata));
+  });
+
+  /**
+   * Basic test for the fetchActivityHistoryForMintAddress function to provide
+   * a sanity check that expected behavior doesn't break.
+   */
+  test("fetchActivityHistoryForMintAddress", async () => {
+    const address = "GPgf9QFoJ3XagLBLWLG9j2Ehtw5ycoXn8hYZuJL4GWQn";
+    const txs = await fetchActivityHistoryForMintAddress(address);
+
+    // Slice final 7 transactions (ignore any possibly newer transactions)
+    const txSlice = txs.slice(txs.length - 7);
+
+    // Remove verbose original data
+    const types = txSlice.map(({ tx, signatures, ...rest }) => rest);
+
+    // Check derived history matches snapshot expectation
+    expect(types).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "seller": "5GUd38kYXm8FN6zkcH4ynBnmQsLZrModzraT9bM3dAYq",
+          "type": "Listing",
+        },
+        Object {
+          "seller": "5GUd38kYXm8FN6zkcH4ynBnmQsLZrModzraT9bM3dAYq",
+          "type": "CancelListing",
+        },
+        Object {
+          "seller": "5GUd38kYXm8FN6zkcH4ynBnmQsLZrModzraT9bM3dAYq",
+          "type": "Listing",
+        },
+        Object {
+          "buyer": "5GUd38kYXm8FN6zkcH4ynBnmQsLZrModzraT9bM3dAYq",
+          "lamports": "11000000000",
+          "type": "Sale",
+        },
+        Object {
+          "seller": "GCJxBzQY26nz1TXdrNVESHEvQLdTMWGttr71Fq3LgGvW",
+          "type": "Listing",
+        },
+        Object {
+          "destinationTokenAccount": "GLiWFWvCQZ7SG6NVHoKq9Yqf2UQfMLcSJzCmU6yEGgFD",
+          "newOwnerAddress": "GCJxBzQY26nz1TXdrNVESHEvQLdTMWGttr71Fq3LgGvW",
+          "source": "3qkgX9qgfP7p2nybgJBFHjyvfysEgzmnURKeXNNtddig",
+          "type": "Transfer",
+        },
+        Object {
+          "minter": "CATPa1APxUC7L3KaWkR2Aiy5LYBThRiNQM1MFrTDC9ZC",
+          "type": "Mint",
+        },
+      ]
+    `);
+  });
+});
